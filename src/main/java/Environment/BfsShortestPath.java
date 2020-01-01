47dc4390945a22dec316796da76578899947ee5f
AbdoBoum <abderrahim.boumahdi@um5s.net.ma>
2020-01-01 18:05:24 +0100

    Refactoring code & adding BFS for shortest path finding

---
+++ src/main/java/Environment/BfsShortestPath.java
@@ -0,0 +1,101 @@
+package Environment;
+
+import Utils.Position;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.*;
+
+public class BfsShortestPath {
+
+    private char[][] map;
+    private Queue<Integer> rQueue;
+    private Queue<Integer> cQueue;
+
+    // to track the number of steps taken
+    private int moveCount;
+    private int nodesLeftInLayer;
+    private int nodesInNextLayer;
+
+    // to process neighbours
+    private boolean[][] visited;
+    private int[] dr = {0, 1, 0, -1, 1, -1, 1, -1};
+    private int[] dc = {1, 0, -1, 0, 1, -1, -1, 1};
+
+    private List<Position> path;
+
+    public BfsShortestPath(char[][] map) {
+        this.map = map;
+    }
+
+    public List<Position> getPath() {
+        if (path.isEmpty()) return Collections.emptyList();
+        return path;
+    }
+
+    public int findShortestPath(Position position) {
+        init();
+        boolean reached = false;
+        int row = position.getRow();
+        int col = position.getCol();
+        rQueue.add(row);
+        cQueue.add(col);
+        visited[row][col] = true;
+        while (!rQueue.isEmpty()) {
+            row = rQueue.poll();
+            col = cQueue.poll();
+            if (atEnd(row, col)) {
+                reached = true;
+                break;
+            }
+            exploreNeighbours(row, col);
+            nodesLeftInLayer--;
+            if (nodesLeftInLayer == 0) {
+                path.add(new Position(row, col));
+                nodesLeftInLayer = nodesInNextLayer;
+                nodesInNextLayer = 0;
+                moveCount++;
+            }
+        }
+        return reached ? moveCount : -1;
+    }
+
+    private void init() {
+        rQueue = new LinkedList<>();
+        cQueue = new LinkedList<>();
+        moveCount = 0;
+        nodesLeftInLayer = 1;
+        nodesInNextLayer = 0;
+        visited = new boolean[map.length][map[0].length];
+        path = new ArrayList<>();
+    }
+
+    private boolean atEnd(int row, int col) {
+        return map[row][col] == 'D';
+    }
+
+    private void exploreNeighbours(int row, int col) {
+        for (int i = 0; i < 8; i++) {
+            int rr = row + dr[i];
+            int cc = col + dc[i];
+            if (isValidMove(rr, cc)) {
+                rQueue.add(rr);
+                cQueue.add(cc);
+                visited[rr][cc] = true;
+                nodesInNextLayer++;
+            }
+        }
+    }
+
+    private boolean isValidMove(int row, int col) {
+        return isInTheGrid(row, col) && !visited[row][col] && !isObstacle(row, col);
+    }
+
+    private boolean isInTheGrid(int row, int col) {
+        return row >= 0 && col >= 0 && row < map.length && col < map[0].length;
+    }
+
+    private boolean isObstacle(int row, int col) {
+        return map[row][col] != '#';
+    }
+}
--- src/test/java/Environment/MiningFieldTest.java
+++ src/test/java/Environment/MiningFieldTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import java.util.stream.IntStream;
 
+import static Environment.MiningField.DEFAULT_GOLD_PIECES;
+import static Environment.MiningField.DEFAULT_OBSTACLES;
 import static Environment.MiningField.MAP_HEIGHT;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertNotNull;
@@ -14,16 +16,16 @@ public class MiningFieldTest {
 
     @Test
     public void defaultEnvironmentCreation() {
-        MiningField miningField = new MiningField();
-        assertThat(miningField.getGoldPieces(), equalTo(30));
-        assertThat(miningField.getObstacles(), equalTo(30));
+        var miningField = new MiningField();
+        assertThat(miningField.getGoldPieces(), equalTo(DEFAULT_GOLD_PIECES));
+        assertThat(miningField.getObstacles(), equalTo(DEFAULT_OBSTACLES));
         assertNotNull(miningField.getMap());
         assertNotNull(miningField.getOwner());
     }
 
     @Test
     public void customEnvironmentCreation() {
-        MiningField miningField = new MiningField(50, 20, new Leader());
+        var miningField = new MiningField(50, 20, new Leader());
         assertThat(miningField.getGoldPieces(), equalTo(50));
         assertThat(miningField.getObstacles(), equalTo(20));
         assertThat(miningField.getMap()[0].length, equalTo(MAP_HEIGHT));
@@ -31,20 +33,20 @@ public class MiningFieldTest {
 
     @Test
     public void countGoldPiecesAndObstacles() {
-        MiningField miningField = new MiningField();
-        long goldPieces = 0L;
-        long obstacles = 0L;
-        long miners = 0L;
-        for (int i = 0; i < MAP_HEIGHT; ++i) {
-            char[] line = miningField.getMap()[i];
+        var miningField = new MiningField();
+        var goldPieces = 0L;
+        var obstacles = 0L;
+        var miners = 0L;
+        for (var i = 0; i < MAP_HEIGHT; ++i) {
+            var line = miningField.getMap()[i];
             goldPieces += IntStream.range(0, line.length)
                     .mapToObj(c -> line[c])
-                    .filter(x -> x == 'O')
+                    .filter(x -> x == 'o')
                     .count();
 
             obstacles += IntStream.range(0, line.length)
                     .mapToObj(c -> line[c])
-                    .filter(x -> x == 'X')
+                    .filter(x -> x == '#')
                     .count();
 
             miners += IntStream.range(0, line.length)
@@ -53,8 +55,8 @@ public class MiningFieldTest {
                     .count();
         }
 
-        assertThat((int)goldPieces, equalTo(30));
-        assertThat((int)obstacles, equalTo(30));
+        assertThat((int)goldPieces, equalTo(DEFAULT_GOLD_PIECES));
+        assertThat((int)obstacles, equalTo(DEFAULT_OBSTACLES));
         assertThat((int)miners, equalTo(4));
 
     }
