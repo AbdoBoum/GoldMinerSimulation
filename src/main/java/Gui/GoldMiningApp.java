dc2e3cfe22b76f45286047c41c8c1ffe8a8c030b
AbdoBoum <abderrahim.boumahdi@um5s.net.ma>
2020-01-28 10:18:12 +0100

    Adding javafx grid

--- src/main/java/Gui/GoldMiningApp.java
+++ src/main/java/Gui/GoldMiningApp.java
@@ -1,55 +1,32 @@
 package Gui;
 
-import com.almasb.fxgl.app.GameApplication;
-import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.app.GameView;
-import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.pathfinding.CellState;
-import com.almasb.fxgl.pathfinding.astar.AStarGrid;
-import com.almasb.fxgl.texture.Texture;
-import javafx.scene.paint.Color;
+import Gui.Components.GridDisplay;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import static Environment.MiningField.MAP_HEIGHT;
 import static Environment.MiningField.MAP_WIDTH;
-import static Gui.EntityType.OBSTACLE;
-import static com.almasb.fxgl.dsl.FXGLForKtKt.*;
 
-public class GoldMiningApp extends GameApplication {
+public class GoldMiningApp extends Application {
 
-    public static final int BLOCK_SIZE = 60;
+    private GridDisplay gridDisplay;
 
     @Override
-    protected void initSettings(GameSettings gameSettings) {
-        gameSettings.setWidth(MAP_HEIGHT * BLOCK_SIZE);
-        gameSettings.setHeight(MAP_WIDTH * BLOCK_SIZE);
-        gameSettings.setTitle("GOLD MINERS");
-        gameSettings.setVersion("0.1");
-        gameSettings.setManualResizeEnabled(true);
-        gameSettings.setPreserveResizeRatio(true);
-    }
+    public void start(Stage primaryStage) throws Exception {
+        gridDisplay = new GridDisplay(MAP_HEIGHT, MAP_WIDTH);
+        BorderPane mainPanel = new BorderPane();
+        mainPanel.setCenter(gridDisplay.getDisplay());
 
-
-    @Override
-    protected void initGame() {
-        getGameScene().setBackgroundColor(Color.SLATEGRAY);
-
-        var miner = FXGL.getAssetLoader().loadTexture("miner.png");
-        miner.setTranslateX(0);
-        miner.setTranslateY(0);
-        FXGL.getGameScene().addUINode(miner);
-
-        var obstacle = FXGL.getAssetLoader().loadTexture("obstacle.png");
-        obstacle.setTranslateX(80);
-        obstacle.setTranslateY(350);
-        FXGL.getGameScene().addUINode(obstacle);
-
-        var gold = FXGL.getAssetLoader().loadTexture("gold.png");
-        gold.setTranslateX(90);
-        gold.setTranslateY(440);
-        FXGL.getGameScene().addUINode(gold);
+        Scene scene = new Scene(mainPanel, 800, 800);
+        primaryStage.setTitle("Gold Mining");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
